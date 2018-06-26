09a1b34b4cbef4c6ee8c4364b764b44e874208f9
tangrobs <bobtang@gmail.com>
2018-06-26 14:23:57 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,80 @@
+const Deck = require('./deck_of_cards').Deck
+const Player = require('./deck_of_cards').Player
+const Card = require('./deck_of_cards').Card
+const Bluff = require('./deck_of_cards').Bluff
+
+
+const express = require('express')
+const app = express();
+app.use(express.static(__dirname+"/public"))
+const server = app.listen(8000,()=>console.log("listening on port 8000"))
+const io = require('socket.io')(server);
+
+
+var bluff = new Bluff()
+
+io.on('connection',function(socket){
+    var thisPlayer
+    //adding player name
+    socket.on("player_name",function(name){
+        thisPlayer = name
+        bluff.add(new Player(name, socket.id))
+
+        socket.emit("yourid",socket.id)
+
+        //let all players know that this player has joined the game
+        io.emit("player_list",bluff.state.players)
+
+        //if there is only one player, make him the leader
+        if(bluff.state.players[0].socketid == socket.id){
+            socket.emit("isleader","hey you are the leader of this game")
+        }
+    })
+    
+    socket.on('startgame',function(d){
+        bluff.start()
+        io.emit("game_state",bluff.state)
+    })
+
+    socket.on('callbluff',function(d){
+        console.log("person called bluff");
+    })
+    
+    socket.on('pass',function(d){
+        console.log("person wants to pass");
+    })
+
+    socket.on('play',function(data){
+        bluff.state.most_recent_hand = {
+            isbluff: false,
+            cards:[],
+            player:null
+        }
+        //can do a check here for curround_card_value
+        bluff.state.curround_card_value = data.choosen_card
+        for(var i in data.selected_cards){
+            curcard = bluff.state.players[bluff.state.active_player].discard(data.selected_cards[i])
+            if(curcard.value != bluff.state.curround_card_value){
+                bluff.state.most_recent_hand.isbluff = true;
+            }
+            bluff.state.most_recent_hand.cards.push(curcard)
+        }
+        bluff.state.most_recent_hand.player = bluff.state.active_player
+        bluff.state.curround_plays.push(bluff.state.most_recent_hand)
+
+        bluff.state.active_player = (bluff.state.active_player + 1) % bluff.state.players.length
+
+        console.log(bluff.state.players[0].hand.length)
+        io.emit("game_state",bluff.state)
+    })
+
+    socket.on('disconnect',function(){
+        for(let i in bluff.state.players){
+            if(bluff.state.players[i].socketid == socket.id){
+                bluff.state.players.splice(i,1)
+                break;
+            }
+        }
+        io.emit("player_list",bluff.state.players)
+    })
+})
